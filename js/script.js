20b983417a7bc22b728715917b090e417ddfc53c
Aaqib Parvez <aaqib29292@gmail.com>
2016-10-26 19:26:20 +0530

    drew enemies and updated it vertical movement

--- js/script.js
+++ js/script.js
@@ -1,15 +1,41 @@
 window.addEventListener('load', function(){
 
-  // intial position
-  var x = 10;
-  var y = 10;
 
-  // rectangular dimensions
-  var w = 20;
-  var h = 30;
+  // Constants
+  var GAME_WIDTH = 640;
+  var GAME_HEIGHT = 360;
 
-  // speed at wch it moves
-  var speed = 2;
+
+  var enemies = [
+    {
+      x: 100, /*x coordinate*/
+      y: 100, /*y coordinate*/
+      speedY: 2, /*speed in Y*/
+      w: 40, /*width*/
+      h: 40 /*height*/
+    },
+    {
+      x: 200, /*x coordinate*/
+      y: 0, /*y coordinate*/
+      speedY: 2, /*speed in Y*/
+      w: 40, /*width*/
+      h: 40 /*height*/
+    },
+    {
+      x: 330, /*x coordinate*/
+      y: 100, /*y coordinate*/
+      speedY: 3, /*speed in Y*/
+      w: 40, /*width*/
+      h: 40 /*height*/
+    },
+    {
+      x: 450, /*x coordinate*/
+      y: 100, /*y coordinate*/
+      speedY: 5, /*speed in Y*/
+      w: 40, /*width*/
+      h: 40 /*height*/
+    },
+  ]
 
   // getting canvas n context
   var canvas = document.getElementById('canvas');
@@ -18,15 +44,29 @@ window.addEventListener('load', function(){
   // updating the rectangle position
   var update = function() {
 
-    x = x + speed;
+    // update the position of each enemy
+    var n = enemies.length;
+
+    enemies.forEach(function (ele, index) {
+      ele.y += ele.speedY;
+    })
 
   };
 
   // show it on the screen
   var draw = function(){
-    ctx.clearRect(0, 0, 500, 300);
+    // clearing the canvas
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     ctx.fillStyle = "rgb(200,0,100)";
-    ctx.fillRect(x, y, w, h);
+
+    //draw each enemy
+
+    var n = enemies.length;
+
+    enemies.forEach(function (ele, index) {
+      ctx.fillRect(ele.x, ele.y, ele.w, ele.h);
+    })
+
   }
 
   // to repeat step fn multiple time per sec
